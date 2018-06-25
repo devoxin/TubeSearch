10307ec53ba9cdce5a76dbb280d2bdb96822d964
Luke <15076404+Devoxin@users.noreply.github.com>
2018-06-25 14:20:21 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const { load } = require('cheerio');
 const { get } = require('snekfetch');
 
+const durationRegex = RegExp('(?:(\\d{1,2}):)?(\\d{1,2}):(\\d{2})');
+
+/**
+ * Searches YouTube for the specified query
+ * @param {string} query The query to search for
+ * @returns {Promise<Array<Object>, Error>} An array of results. Could be empty if nothing found
+ */
 async function search (query) {
   const data = await get(`https://youtube.com/results?search_query=${encodeURIComponent(query)}`);
   const $ = load(data.body.toString());
@@ -12,15 +19,44 @@ async function search (query) {
 
   const videos = $(results).each((i, el) => {
     const info = $(el).find('h3.yt-lockup-title a');
-    const link = info.attr('href');
+    const duration = $(el).find('span.video-time').text();
+    const durationMs = getDurationMs(duration);
+    const uploader = $(el).find('div.yt-lockup-byline a').text();
+    const link = `https://www.youtube.com${info.attr('href')}`;
 
     items.push({
       title: info.text(),
-      id: link.substring(link.indexOf('=') + 1)
+      link,
+      id: link.substring(link.indexOf('=') + 1),
+      uploader,
+      duration,
+      durationMs
     });
   });
 
   return items;
+}
+
+
+/**
+ * Converts a given time-string into milliseconds
+ * @param {string} timeString The string to convert to milliseconds
+ * @returns {(Number|Null)} Milliseconds if successful, otherwise null
+ */
+function getDurationMs (timeString) {
+  const match = durationRegex.exec(timeString);
+
+  if (!match) {
+    return null;
+  }
+
+  const hours = Number(match[1]) || 0;
+  const minutes = Number(match[2]) || 0;
+  const seconds = Number(match[3]) || 0;
+
+  const ms = (hours * 3600000) + (minutes * 60000) + (seconds * 1000); // eslint-disable-line
+
+  return ms;
 }
 
 module.exports = search;
