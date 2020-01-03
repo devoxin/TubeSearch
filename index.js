41ef280db23dafa3c252ca7bae0668c2775f292a
Devoxin <15076404+Devoxin@users.noreply.github.com>
2020-01-03 18:13:49 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const { load } = require('cheerio');
-const { get } = require('snekfetch');
+const fetch = require('node-fetch');
+
 
 /**
  * Searches YouTube for the specified query.
  * @param {String} query The query to search for.
- * @param {Number} limit The maximum amount of results to return.
- * @returns {Promise<Array<Result>, Error>} An array of results. Could be empty if nothing found.
+ * @param {Number} limit The maximum amount of videos to return.
+ * @returns {Promise<Array<Video>, Error>} An array of videos. Could be empty if nothing found.
  */
 async function search (query, limit) {
-  const data = await get(`https://youtube.com/results?search_query=${encodeURIComponent(query)}`);
-  const $ = load(data.body.toString());
+  const data = await fetch(`https://youtube.com/results?search_query=${encodeURIComponent(query)}`)
+    .then(res => res.text());
+  const $ = load(data);
 
   const results = $('.yt-lockup-video')
     .filter((_, e) => !('data-ad-impressions' in e.attribs));
@@ -41,6 +43,46 @@ async function search (query, limit) {
   return items;
 }
 
+/**
+ * Retrieves the videos of the playlist with the given identifier.
+ * Limited to the first 100 videos.
+ * @param {String} identifier The playlist's unique identifier.
+ * @param {Number} limit The maximum amount of videos to return.
+ * @returns {Promise<Array<Video>, Error>} An array of videos. Could be empty if nothing found.
+ */
+async function playlist (identifier, limit) {
+  const data = await fetch(`https://youtube.com/playlist?list=${identifier}`)
+    .then(res => res.text());
+  const $ = load(data);
+
+  const results = $('.pl-video')
+  const items = [];
+
+  results.each((_, el) => {
+    const selector = $(el);
+    const info = selector.find('.pl-video-title-link');
+    const duration = selector.find('.pl-video-time .timestamp').text();
+    const durationMs = getDurationMs(duration);
+    const uploader = selector.find('.pl-video-owner a').text();
+    const link = `https://youtube.com${info.attr('href')}`;
+
+    items.push({
+      title: info.text().trim(),
+      link,
+      id: link.substring(link.indexOf('=') + 1, link.indexOf('&')),
+      uploader,
+      duration,
+      durationMs
+    });
+  });
+
+  if (limit && Number(limit)) {
+    items.length = Number(limit);
+  }
+
+  return items;
+}
+
 
 /**
  * Converts a given time-string into milliseconds.
@@ -48,7 +90,7 @@ async function search (query, limit) {
  * @returns {(Number|Null)} Milliseconds if successful, otherwise null.
  */
 function getDurationMs (timeString) {
-  const parts = timeString.split(':');
+  const parts = timeString.split(':').map(Number);
 
   if (parts.length === 3) {
     const [hours, minutes, seconds] = parts;
@@ -61,14 +103,17 @@ function getDurationMs (timeString) {
   }
 }
 
-module.exports = search;
+module.exports = {
+  playlist,
+  search
+};
 
 /**
- * @typedef {Object} Result
- * @property {String} title The title of the result.
- * @property {String} link The direct URL of the result.
+ * @typedef {Object} Video
+ * @property {String} title The title of the video.
+ * @property {String} link The direct URL to the video.
  * @property {String} id The identifier of the video.
- * @property {String} uploader The name of the channel that uploaded the result.
- * @property {String} duration The humanized duration of the result.
+ * @property {String} uploader The name of the channel that uploaded the video.
+ * @property {String} duration The humanized duration of the video.
  * @property {Number} durationMs The duration in milliseconds.
  */
